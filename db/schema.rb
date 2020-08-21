e9cc3456e309c9e76257b8f250aab38e96d43993
João Vitor Araujo <joao18araujo@gmail.com>
2020-08-21 20:47:09 -0300

    Create University model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_08_21_234524) do
+
+  create_table "universities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
