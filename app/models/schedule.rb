2332b72970d307f37412c58caf3b37c85b8629d9
João Vitor Araujo <joao18araujo@gmail.com>
2020-09-06 14:54:20 -0300

    Create application and users controller

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,4 +4,9 @@ class Schedule < ApplicationRecord
 
   has_many :subject_class_has_schedules, dependent: :destroy
   has_many :subject_classes, through: :subject_class_has_schedules
+
+  def start_time=(time)
+    time = '1996-1-' + time if time.try(:match?, /\d \d{2}:\d{2}/)
+    super(time)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+unb = University.create(name: 'UnB')
+gama = Campus.create(name: 'Gama', university: unb)
+fga = Department.create(name: 'FGA', campus: gama)
+swe = Course.create(name: 'Engenharia de Software', campus: gama)
+ije = Subject.create(name: 'Introdução a Jogos Eletrônicos', department: fga)
+a_class = SubjectClass.create(name: 'A', subject: ije)
+tuesday_14_to_16 = Schedule.create(start_time: '', end_time: '')
+thursday_14_to_16 = Schedule.create(start_time: '', end_time: '')
